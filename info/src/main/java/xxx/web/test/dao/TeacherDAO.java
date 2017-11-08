ce37c9295de6b1b2d8bec06702bf36a2b9cf4c14
xiajunhua <18810796504@163.com>
2017-11-08 16:00:08 +0800

    老师DAO

---
+++ info/src/main/java/xxx/web/test/dao/TeacherDAO.java
@@ -0,0 +1,12 @@
+package xxx.web.test.dao;
+
+import java.util.List;
+
+import xxx.web.test.bean.Teacher;
+
+public class TeacherDAO {
+
+	public List<Teacher> getStudentList(){
+		return null;
+	}
+}
