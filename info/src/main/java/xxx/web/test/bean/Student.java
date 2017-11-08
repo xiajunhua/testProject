ff4a361a8ed5ff46d67eaecd0b1186c5671c142d
xiajunhua <18810796504@163.com>
2017-11-08 15:56:37 +0800

    学生 老师 的实体类

---
+++ info/src/main/java/xxx/web/test/bean/Student.java
@@ -0,0 +1,48 @@
+package xxx.web.test.bean;
+
+public class Student {
+
+	private Integer stuNo;
+	private String stuName;
+	private String sex;
+	private String phone;
+	private String address;
+	private Integer age;
+	public Integer getStuNo() {
+		return stuNo;
+	}
+	public void setStuNo(Integer stuNo) {
+		this.stuNo = stuNo;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
---
+++ info/src/main/java/xxx/web/test/bean/Teacher.java
@@ -0,0 +1,48 @@
+package xxx.web.test.bean;
+
+public class Teacher {
+
+	private Integer teaNo;
+	private String teaName;
+	private String sex;
+	private String phone;
+	private String address;
+	private Integer age;
+	public Integer getTeaNo() {
+		return teaNo;
+	}
+	public void setTeaNo(Integer teaNo) {
+		this.teaNo = teaNo;
+	}
+	public String getTeaName() {
+		return teaName;
+	}
+	public void setTeaName(String teaName) {
+		this.teaName = teaName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
