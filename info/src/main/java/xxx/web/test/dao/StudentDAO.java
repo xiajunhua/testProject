1e4fe7bc139654506b57fa2945beea4b8cb11e5b
xiajunhua <18810796504@163.com>
2017-11-08 15:59:38 +0800

    学生DAO

---
+++ info/src/main/java/xxx/web/test/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+package xxx.web.test.dao;
+
+import java.util.List;
+
+import xxx.web.test.bean.Student;
+
+public class StudentDAO {
+
+	public List<Student> getStudentList(){
+		return null;
+	}
+}
