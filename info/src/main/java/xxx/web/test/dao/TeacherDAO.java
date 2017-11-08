e06796eeb830d9f3a9b13d9ae480b6d331126b08
xiajunhua <18810796504@163.com>
2017-11-08 16:06:18 +0800

    修改TeacherDAO

--- info/src/main/java/xxx/web/test/dao/TeacherDAO.java
+++ info/src/main/java/xxx/web/test/dao/TeacherDAO.java
@@ -2,11 +2,15 @@ package xxx.web.test.dao;
 
 import java.util.List;
 
+import xxx.web.test.bean.Student;
 import xxx.web.test.bean.Teacher;
 
 public class TeacherDAO {
 
 	public List<Teacher> getStudentList(){
 		return null;
+	}
+	public void addTea(Teacher tea){
+		
 	}
 }
